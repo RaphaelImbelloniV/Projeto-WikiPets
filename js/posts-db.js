2f27c1e9c4677d7219b16c79d02407966f9e5bde
renato ramos <bobrenato998@gmail.com>
2025-06-10 11:29:15 -0300

    componente de recomendação de posts
    criação de um componente reutilizavel para recomendar os outros posts do site

---
+++ js/posts-db.js
@@ -0,0 +1,25 @@
+
+
+const allPosts = [
+    {
+        id: 1,
+        title: 'A Importância de Doar para Abrigos de Animais',
+        url: '/posts/ong.html',
+        image: '/imgs/ong.png', 
+        description: 'A doação para abrigos desempenha um papel fundamental em garantir o bem-estar e o futuro de animais indefesos.'
+    },
+    {
+        id: 2,
+        title: 'Guia para Donos de Gatinhos',
+        url: '/posts/gatos.html',
+        image: '/imgs/thumb-gatinhos.jpg',
+        description: 'Este guia abrange as necessidades essenciais do seu felino e dicas para criar um ambiente saudável e feliz.'
+    },
+    {
+        id: 3,
+        title: 'A Importância da Hidratação para Seu Pet',
+        url: '/posts/agua.html', // Nome do arquivo para este novo post
+        image: '/imgs/thumb-hidratacao.jpg', // Crie uma imagem com este nome e caminho
+        description: 'A água é essencial para a saúde e bem-estar do seu pet, ajudando a manter suas funções vitais.'
+    }
+];
