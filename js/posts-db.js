477ba56efa7be75eb1745ebf3506e0250337cd8c
caioznnn <ccaio7007@gmail.com>
2025-11-24 09:01:41 -0300

    refatorado os nomes dos arquivos - refs #43

--- js/posts-db.js
+++ js/posts-db.js
@@ -4,21 +4,21 @@ const allPosts = [
     {
         id: 1,
         title: 'A Importância de Doar para Abrigos de Animais',
-        url: './ong.html',
+        url: './doacaoabrigoanimais.html',
         image: 'https://wikipetsuni.netlify.app/static/media/ong.5796b8835fae846a146e.png', 
         description: 'A doação para abrigos desempenha um papel fundamental em garantir o bem-estar e o futuro de animais indefesos.'
     },
     {
         id: 2,
         title: 'Guia para Donos de Gatinhos',
-        url: './gatos.html',
+        url: './guiadonogatos.html',
         image: 'https://www.purina.co.uk/sites/default/files/2023-03/Teaser%20Pedigree%20Cats.jpg',
         description: 'Este guia abrange as necessidades essenciais do seu felino e dicas para criar um ambiente saudável e feliz.'
     },
     {
         id: 3,
         title: 'A Importância da Hidratação para Seu Pet',
-        url: './agua.html',
+        url: './paginahidratacao.html',
         image: 'https://siwi.org/wp-content/uploads/2021/09/colorful-water-drop-splash-e1635164525186.jpg', 
         description: 'A água é essencial para a saúde e bem-estar do seu pet, ajudando a manter suas funções vitais.'
     }
